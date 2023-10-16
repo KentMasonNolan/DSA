aa1f2a3e8b6abc79b2e789999a7b32fcad0ebe0f
Kent Nolan <kent.nolan@gmail.com>
2023-10-17 10:19:13 +1300

    DSA update

--- Assignment2/noiseremoving/CollectionSort.java
+++ Assignment2/noiseremoving/CollectionSort.java
@@ -1,100 +1,82 @@
 package Assignment2.noiseremoving;
 
-
-
-/**
+/*
  * Question 1:
  *      Is quick sort the best way of finding median? Why?
  * Answer:
- *      No, quick sort is not the best way because it has the time complexity of O(n log n) and
- *      requires more methods to make it work. Whereas there are more efficient algorithms.
+ *      Quick sort isn't necessarily the most efficient method for identifying the median. While it sorts
+ *      with a time complexity of O(n log n), there are specialized techniques designed specifically for
+ *      finding the median without the need to sort the entire array.
  *
  * Question 2:
  *      What is another good way of finding median? Please provide your explanation.
  * Answer:
- *      Insertion sort and selection sort are both good ways for finding the median
- *      in a small window of 9 pixels. They have the time complexity of O(n^2), however,
- *      due to the small window size, their performance impact is minimal. Both algorithms
- *      are simple to make and can sort the small window more efficiently.
+ *      In the context of a limited dataset, such as a 9-pixel window, algorithms like insertion sort
+ *      and selection sort can be quite effective. Their O(n^2) time complexity might seem high, but
+ *      given the small dataset, the real-world performance is acceptable. Their inherent simplicity
+ *      ensures a fast and efficient sort for small data sizes.
  *
  */
-public class CollectionSort <E extends Comparable> {
+
+public class CollectionSort<E extends Comparable> {
 
     public E[] array;
 
     /**
-     * Takes a reference of an array and passes the reference to the "array" field.
-     *
-     * @param array
+     * Receives an array reference and assigns it to the class's "array" field.
      */
-    public void setArray(E[] array)
-    {
+    public void setArray(E[] array) {
         this.array = array;
     }
 
     /**
-     * Runs quick sort algorithm and sorts array in order using quickSort() recursive method.
+     * Executes the quick sort algorithm to order the array elements.
      */
-    public void quickSort()
-    {
-        if (array == null || array.length <= 1)
-        {
+    public void quickSort() {
+        if (array == null || array.length == 0 || array.length == 1) {
             return;
         }
         quickSort(0, array.length - 1);
     }
 
     /**
-     * Recursively performs the quick sort algorithm on the array.
-     *
-     * @param low (The lower index of the array)
-     * @param high (The higher index of the array)
+     * Implements the recursive quick sort mechanism on the given array segment.
+     * Sorts the segment of the array between the provided starting and ending indices.
      */
-    private void quickSort(int low, int high)
-    {
-        if (low < high)
-        {
-            int pivotIndex = partition(low, high);
-            quickSort(low, pivotIndex - 1);
-            quickSort(pivotIndex + 1, high);
+    private void quickSort(int low, int high) {
+        if (low < high) {
+            int pivotPosition = partition(low, high);
+            quickSort(low, pivotPosition - 1);
+            quickSort(pivotPosition + 1, high);
         }
     }
 
     /**
-     * Partitions the array based on a pivot element.
-     *
-     * @param low (The lower index of the array)
-     * @param high (The higher index of the array)
-     * @return The index of the pivot element
+     * Establishes a pivot in the array and organizes elements based on their relation to it.
+     * Organizes elements of the array around a selected pivot, ensuring values less than the pivot come before it, and values greater come after.
      */
-    private int partition(int low, int high)
-    {
-        E pivot = array[high];
+    private int partition(int low, int high) {
+        E pivotValue = array[high];
         int i = low - 1;
 
-        for (int j = low; j < high; j++)
-        {
-            if (array[j].compareTo(pivot) <= 0)
-            {
+        for (int j = low; j < high; j++) {
+            if (array[j].compareTo(pivotValue) <= 0) {
                 i++;
-                swap(i, j);
+                swapElements(i, j);
             }
         }
 
-        swap(i + 1, high);
+        swapElements(i + 1, high);
         return i + 1;
     }
 
     /**
-     * Swaps two elements in the array.
-     *
-     * @param i (The index of the first element)
-     * @param j (The index of the second element)
+     * Exchanges positions of two elements within the array.
+     * Switches the places of two array elements based on provided indices.
      */
-    private void swap(int i, int j)
-    {
-        E temp = array[i];
+    private void swapElements(int i, int j) {
+        E placeholder = array[i];
         array[i] = array[j];
-        array[j] = temp;
+        array[j] = placeholder;
     }
 }
