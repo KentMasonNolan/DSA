f9e682713077df140c76b81d36cf7f295d7a3ca5
Kent Nolan <kent.nolan@gmail.com>
2023-10-17 21:25:07 +1300

    Adding comments

--- Assignment2/studentapp/StudentManager.java
+++ Assignment2/studentapp/StudentManager.java
@@ -19,11 +19,16 @@ public class StudentManager<E,F  extends Comparable> {
         this.bTreeName = new BinaryTree<>();
     }
 
+    // method to add a new student to both score and name binary trees
     public void addStudent(float score, String name, String comments) {
+        // create a new student instance with the provided details
         Student student = new Student(name, score, comments); // Assuming you have a Student class defined with appropriate constructor
+        // add student to the score-based binary tree
         addToTree(student, score); // Add to bTreeScore
+        // add student to the name-based binary tree
         addToTree(student, name); // Add to bTreeName
     }
+
 
     public void addToTree(Student student, Float score) {
         bTreeScore.addElement(student, score);
@@ -33,55 +38,53 @@ public class StudentManager<E,F  extends Comparable> {
         bTreeName.addElement(student, name);
     }
 
-//    public Student findStudent(E key) {
-//        if (key instanceof Float) {
-//            return bTreeScore.searchElement((Float) key);
-//        } else if (key instanceof String) {
-//            // If the key is a String, we assume it's a name and perform a full tree search
-//            return searchStudentByName(bTreeName.root, (String) key);
-//        }
-//        return null;
-//    }
 
+    // method to find a student using a given key (can be either a Float for score or a String for name)
     public Student findStudent(F key) {
+        // if the key is a Float, search in the score-based binary tree
         if (key instanceof Float) {
             for (int i = 0; i < bTreeScore.nodeList.length; i++) {
+                // if key matches, return the student from the node
                 if (bTreeScore.nodeList[i].key.equals(key)) {
                     Student student = (Student) bTreeScore.nodeList[i].element;
                     return student;
                 }
             }
+            // if the key is a String, search in the name-based binary tree
         } else if (key instanceof String) {
             for (int i = 0; i < bTreeName.nodeList.length; i++) {
+                // if key matches, return the student from the node
                 if (bTreeName.nodeList[i].key.equals(key)) {
                     Student student = bTreeName.nodeList[i].element;
                     return student;
                 }
             }
         }
+        // if student is not found, return null
         return null;
     }
 
 
+
+    // method to search for a student by their name in the binary tree
     public Student searchStudentByName(Node root, String name) {
+        // base case: if the node is null, return null
         if (root == null) {
             return null;
         }
-
-        // Assuming 'element' is of type 'Student' and 'name' is a public field.
-        // Cast 'element' to 'Student' and compare 'name' field directly.
+        // cast the node's element to Student to access its name
         Student student = (Student) root.element;
+        // if the student's name matches the search name, return the student
         if (student.name.equals(name)) {
             return student;
         }
-
-        // Then check the left subtree
+        // recursively search the left subtree
         Student foundStudent = searchStudentByName(root.left, name);
+        // if a student is found in the left subtree, return them
         if (foundStudent != null) {
             return foundStudent;
         }
-
-        // Then check the right subtree
+        // if not found in left, recursively search the right subtree
         return searchStudentByName(root.right, name);
     }
 
@@ -90,14 +93,29 @@ public class StudentManager<E,F  extends Comparable> {
 
 
     public Student[] getSortedStudentList(E key) {
-        BinaryTree<Student, ?> tree = (key instanceof Float) ? bTreeScore : bTreeName;
+        // determine which tree to use based on the type of key
+        BinaryTree<Student, ?> tree;
+        if (key instanceof Float) {
+            // if key is a score (float), use score tree
+            tree = bTreeScore;
+        } else {
+            // otherwise, use name tree
+            tree = bTreeName;
+        }
+        // convert the tree to a sorted list of nodes
         Node<Student, ?>[] nodes = tree.toSortedList();
+
+        // prepare an array to hold just the student objects
         Student[] students = new Student[nodes.length];
+        // loop through the nodes, extracting the student object from each one
         for (int i = 0; i < nodes.length; i++) {
             students[i] = nodes[i].element;
         }
+        // return the sorted list of students
         return students;
     }
+
+
     public void reverseOrder() {
         bTreeScore.reverseOrder();
         bTreeName.reverseOrder();
